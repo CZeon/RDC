01ec3da1563f4a45445ab511e911cc13713d783c
Maikon Alexander de Barros Dias <maikonalexander.mabd@gmail.com>
2018-12-05 16:05:37 -0200

    Add files via upload

--- client_udp.c
+++ client_udp.c
@@ -4,10 +4,17 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 #include<stdio.h>
+#include<time.h>
 void error(char *);
 
+void delay(int number_of_seconds){
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds);
+}
+
 int main(int argc, char *argv[]){
-    int sock, length, n, n2, flag = 0;
+    int sock, length, n, n2, flag = 0,cont = 0;
     struct sockaddr_in server, from;
     struct hostent *hp;
     char buffer [256];
@@ -44,14 +51,22 @@ int main(int argc, char *argv[]){
     if(n < 0){
         printf("Mensagem enviada invalida");
     }
-    n2 = recvfrom(sock, buffer, 256, 0, &from, &length);
+    n2 = recvfrom(sock, buffer, 256, 0, &from, &length); //-2;
 
     if(n2 < 0){
         flag = 0;
+    }else{
+        cont = 5;
+    }
+    if(cont == 4){
+        flag = 1;
+        printf("Falha ao enviar Mensagem!");
     }
 
     write(1, "Got an ack: ", 12);
     write(1, buffer, n);
+    delay(1000);
+    cont++;
     }while(flag == 0);
 }
 
